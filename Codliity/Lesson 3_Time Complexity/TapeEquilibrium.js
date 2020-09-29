b49a66b60a613406e277af26af610e248ab9e901
KimTed <bbielo20@gmail.com>
2020-09-29 19:01:59 +0900

    feat: lesson3_3 풀기

---
+++ Codliity/Lesson 3_Time Complexity/TapeEquilibrium.js
@@ -0,0 +1,56 @@
+'use strict'
+/**
+ A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+ Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+ The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+ In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+ For example, consider array A such that:
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+ We can split this tape in four places:
+    P = 1, difference = |3 − 10| = 7
+    P = 2, difference = |4 − 9| = 5
+    P = 3, difference = |6 − 7| = 1
+    P = 4, difference = |10 − 3| = 7
+ Write a function:
+ function solution(A);
+ that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+ For example, given:
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+ the function should return 1, as explained above.
+ Write an efficient algorithm for the following assumptions:
+ N is an integer within the range [2..100,000];
+each element of array A is an integer within the range [−1,000..1,000].
+*/
+
+const solution = arr=> {
+    const resultArr = [];
+
+    for (let i = 0, len = arr.length ; i < len ; i++) {
+        let berfore = 0;
+        let after = 0;
+
+        for (let j = 0 ; j <= i ; j++) berfore += arr[j];
+        for (let k = (i+1) ; k < len ; k++) after += arr[k];
+        resultArr.push(Math.abs(berfore - after));
+    }
+
+    return Math.min(...resultArr);
+}
+
+(() => {
+   /** Test */
+   try {
+       console.log(solution([3,1,2,4,3])); // 1
+   } catch(err) {
+       console.err(err)
+   }
+   
+})();
--- Codliity/Lesson 3_Time Complexity/note.md
+++ Codliity/Lesson 3_Time Complexity/note.md
@@ -6,6 +6,7 @@
   > some()은 조건이 true가 되는 순간 break;
 - Array.prototype.every
 - Array.prototype.fill
+- Math.abs
 ---
 ### TODO list
 - [PermMissingElem]
@@ -13,3 +14,6 @@
    <br><img src = "./[PermMissingElem]result1.png" width="80px">
    <br><img src = "./[PermMissingElem]result2.png" width="80px">
    <br><img src = "./[PermMissingElem]result3.png" width="80px">
+- [TapeEquilibrium]
+   - 100점 만들기<br> 결과: 
+   <br><img src = "./[TapeEquilibrium]result1.png" width="80px">
